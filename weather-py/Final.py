da2a8ff5cefc838ca497c0914b05fd2e8d6b949e
ilmentore72 <s.akhilkrishna@gmail.com>
2020-05-30 16:10:07 +0530

    Updated to version 2

--- weather-py/Final.py
+++ weather-py/Final.py
@@ -1,14 +1,16 @@
 import requests
-from pprint import pprint
 import json
 
 city = input('Enter your city : ')
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=11e295de4ad9d50b9db153edd02054e5&units=metric'.format(city)
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=******ENTER YOUR API KEY HERE****&units=metric'.format(city)
 
 res = requests.get(url)
 
 data = res.json()
+#print(json.dumps(data))
+temperature=data['main']['temp']
 try:
-	print("The weather condition in city : {} is {} \n The average temperature is {} degree celcius".format(city,data['weather'][0]['description'],data['main']['temp']))
+	print("The weather condition in city : {} is {} \nThe temperature in Celcius is {} degree C".format(city,data['weather'][0]['description'],temperature))
+	print("Temperature in Fahreinheit = {} degree F ".format(temperature*1.8+32)) 
 except:print("Invalid city name")
